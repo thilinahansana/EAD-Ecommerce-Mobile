f326fa0790eb7390f3d5b5a6f8385dc6a595062d
it21167850 <it21167850@my.sliit.lk>
2024-10-04 00:24:25 +0530

    add drwable

--- app/src/main/java/com/example/shopx/service/ApiService.kt
+++ app/src/main/java/com/example/shopx/service/ApiService.kt
@@ -6,12 +6,12 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("Products")
+    @GET("products/all")
     suspend fun getProducts(): List<Product>
 }
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://[2402:d000:a500:f62:11f7:6b65:8ec4:58a8]/api/v1/"
+    private const val BASE_URL = "http://[2402:d000:a500:1bf1:a453:c841:56d8:19b1]/api/v1/vendor/"
 
     val api: ApiService by lazy {
         Retrofit.Builder()
