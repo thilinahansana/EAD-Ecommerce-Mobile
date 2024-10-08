7695eddebb00c4f5ac22d0084abc81cbedf01c16
Bathiya Pathum <99188331+bathiyapathum@users.noreply.github.com>
2024-10-08 23:38:49 +0530

    Update OrderActivity.kt

--- app/src/main/java/com/example/shopx/OrderActivity.kt
+++ app/src/main/java/com/example/shopx/OrderActivity.kt
@@ -45,49 +45,7 @@ class OrderActivity : AppCompatActivity() {
 
     }
 
-    private fun fetchOrderHistory(userId: String) {
-        apiService.getOrderHistory(userId).enqueue(object : Callback<List<Order>> {
-            override fun onResponse(call: Call<List<Order>>, response: Response<List<Order>>) {
-                Log.i("String", "History response: $response")
-                if (response.isSuccessful) {
-                    val orders = response.body() ?: emptyList()
-                    setupRecyclerView(orders)
-                } else {
-                    Toast.makeText(this@OrderActivity, "Failed to load order history", Toast.LENGTH_SHORT).show()
-                }
-            }
 
-            override fun onFailure(call: Call<List<Order>>, t: Throwable) {
-                Toast.makeText(this@OrderActivity, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-    private fun setupRecyclerView(orders: List<Order>) {
-
-        val adapter = OrderAdapter(this, orders,
-            { order -> // On track order click
-                val intent = Intent(this, OrderTrackingActivity::class.java).apply {
-                    putExtra("ORDER_ID", order.orderId)
-                    putExtra("ORDER_STATUS", order.status)
-                    putExtra("TRACKING_DETAILS", order.note)
-                    putExtra("ITEM_LIST" , ArrayList(order.items))
-                }
-                startActivity(intent)
-            },
-            { orderId -> // On cancel order click
-                showCancellationDialog(orderId)
-            }
-        )
-        binding.ordersRecyclerView.adapter = adapter
-    }
-
-    private fun showCancellationDialog(orderId: String) {
-        CancellationDialog(this) { note ->
-            // After note is entered, call the cancelOrder API
-            cancelOrder(orderId, note)
-        }.show()
-    }
 
     private fun cancelOrder(orderId: String, requestNote: String) {
 
